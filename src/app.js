042233c68f304114a03f097e5d5031737df1d3fd
Yasmini Gyawali <gyawaliyasmini246@gmail.com>
2023-07-23 11:00:11 +0545

    mongodb use

--- src/app.js
+++ src/app.js
@@ -2,25 +2,73 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.use(cors());  //middleware
+app.use(cors()); //middleware
 
 const PORT = 5000;
 
-app.get("/", (req,res) => {
-    res.status(200).send("This is response from BE");
+//mongodb connection string
+const mongoDbURI = "mongodb://127.0.0.1:27017/lec";
+mongoose.connect(mongoDbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.get("/api/v1/posts",(req,res) => {
-    const posts =fs.readFileSync("./data/posts.json","utf-8").toString();
-    res.status(200).send(posts);
+const userSchema = new mongoose.Schema({
+  email: String,
+  username: String,
+  fullname: String,
+  title: String,
+  skills: [{ type: String }],
+  address: String,
+  job_type: String,
+  id: Number,
+  is_active: Boolean,
+  followers: [{ type: String }],
+  followings: [{ type: String }],
 });
 
-app.get("/api/v1/user",(req,res) => {
-    const user =fs.readFileSync("./data/user.json","utf-8").toString();
-    res.status(200).send(user);
+const User = mongoose.model("User", userSchema);
+
+User.createCollection()
+  .then((col) => {
+    console.log("Collection", col, "created");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+User.create({
+  email: "test@gmail.com",
+  username: "yasi",
+  fullname: "Yasmini Gyawali",
+  title: "Software Developer",
+  skills: ["JS", "PHP", "Java"],
+  address: "Kathmandu Nepal",
+  job_type: "Full Time",
+  id: 1,
+  is_active: true,
+  followers: ["username123", "user123", "user543"],
+  followings: ["username123", "user123", "user543", "user55"],
+}).then(() => {
+  console.log("User Created");
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("This is response from BE");
+});
+
+app.get("/api/v1/posts", (req, res) => {
+  const posts = fs.readFileSync("./data/posts.json", "utf-8").toString();
+  res.status(200).send(posts);
+});
+
+app.get("/api/v1/user", (req, res) => {
+  const user = fs.readFileSync("./data/user.json", "utf-8").toString();
+  res.status(200).send(user);
 });
 
 app.listen(PORT, () => {
-    console.log("App is running on port" + PORT);
+  console.log("App is running on port" + PORT);
 });
